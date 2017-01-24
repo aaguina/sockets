a87d75a902e7ac4949869eb1fa2c49ad44bd2074
Alexander Aguina <aaguina@gmail.com>
2017-01-24 15:45:52 -0200

    Add message to browser UI

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ io.on('connection', function(socket) {
 
   socket.on('message', function(message) {
     console.log('Message received: ' + message.text);
-    socket.broadcast.emit('message', message);
+    io.emit('message', message);
+    // This sends to everyone but the sender
+    // socket.broadcast.emit('message', message);
   });
 
   socket.emit('message', {
