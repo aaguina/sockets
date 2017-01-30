18dacc382352f14012872d91ffb0713a58e0e14b
Alexander Aguina <aaguina@gmail.com>
2017-01-30 16:26:08 -0200

    Added Styles for index and chat page

--- public/js/app.js
+++ public/js/app.js
@@ -19,13 +19,15 @@ socket.on('connect', function() {
 socket.on('message', function(message) {
   console.log('New message: ' + message.text);
   // console.log(message.text);
-  var $message = jQuery('.messages');
+  var $messages = jQuery('.messages');
+  var $message = jQuery('<li class="list-group-item"></li>');
 
   $message.append('<p> <strong>' + message.name + ' ' +
     moment.unix(message.timestamp).format("MMM Do YYYY, h:mm a") +
     ': </strong>');
 
   $message.append('<p>' + message.text + '</p>');
+  $messages.append($message);
 });
 
 // Handles submitting of new messages from UI
--- server.js
+++ server.js
@@ -9,10 +9,47 @@ var now = moment().format("X");
 
 var clientInfo = {};
 
+// Sends current users to provided socket
+function sendCurrentUsers(socket) {
+  var userData = clientInfo[socket.id];
+  var users = [];
+  if (typeof userData === 'undefined') {
+    return;
+  }
+  Object.keys(clientInfo).forEach(function(socketId) {
+    var userInfo = clientInfo[socketId];
+    if (userData.room === userInfo.room) {
+      users.push(userInfo.name);
+    }
+  });
+
+  socket.emit('message', {
+    name: 'System',
+    text: 'Current Users: ' + users.join(', '),
+    timestamp: now
+  });
+}
+
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket) {
   console.log('User connected via socket.io');
+
+  socket.on('disconnect', function() {
+    var userData = clientInfo[socket.id];
+    console.log('User disconnected:');
+
+    if (typeof userData !== 'undefined') {
+      io.to(userData.room).emit('message', {
+        text: 'User ' + userData.name + ' has left!',
+        timestamp: now,
+        name: 'System'
+      });
+
+      socket.leave(userData.room);
+      delete clientInfo[socket.id];
+    }
+  });
 
   socket.on('joinRoom', function(req) {
     clientInfo[socket.id] = req;
@@ -26,11 +63,16 @@ io.on('connection', function(socket) {
 
   socket.on('message', function(message) {
     console.log('Message received: ' + message.text);
-    // Adds timestamp to message object
-    message.timestamp = now;
-    io.to(clientInfo[socket.id].room).emit('message', message);
-    // This sends to everyone but the sender
-    // socket.broadcast.emit('message', message);
+
+    if (message.text === '@currentUsers') {
+      sendCurrentUsers(socket);
+    } else {
+      // Adds timestamp to message object
+      message.timestamp = now;
+      io.to(clientInfo[socket.id].room).emit('message', message);
+      // This sends to everyone but the sender
+      // socket.broadcast.emit('message', message);
+    }
   });
 
   socket.emit('message', {
